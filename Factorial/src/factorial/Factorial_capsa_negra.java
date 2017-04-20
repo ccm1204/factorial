ee6e60684b72f62ac407a69804319378fa4dd82e
cfgs <cfgs@pc-info-host>
2017-04-20 19:54:26 +0200

    qwdf

--- Factorial/src/factorial/Factorial_capsa_negra.java
+++ Factorial/src/factorial/Factorial_capsa_negra.java
@@ -9,6 +9,14 @@ public class Factorial_capsa_negra {
 		return n * factorial(n - 1);
 	}
 
+	private static int factorialIterativo( int n ) {
+		int res=1;
+		for(int i=2; i<=n; i++) {
+			res=res*i;
+		}
+		return res;
+	}
+
 	public static void main(String[] args){
 		System.out.println(factorial(Integer.parseInt(args[0])));
 	}
