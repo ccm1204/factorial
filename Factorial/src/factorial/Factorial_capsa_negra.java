07b04b8513a2acedf9c1bb417d73daf76e5c84d1
crist <crist@MSI-CCM>
2017-04-24 17:27:06 +0200

    Prova

--- Factorial/src/factorial/Factorial_capsa_negra.java
+++ Factorial/src/factorial/Factorial_capsa_negra.java
@@ -9,16 +9,6 @@ public class Factorial_capsa_negra {
 		return n * factorial(n - 1);
 	}
 
-	// markos cambia s coasas 2 veses
-	
-	private static int factorialIterativo( int n ) {
-		int res=1;
-		for(int i=2; i<=n; i++) {
-			res=res*i;
-		}
-		return res;
-	}//asdfasdf
-
 	public static void main(String[] args){
 		System.out.println(factorial(Integer.parseInt(args[0])));
 	}
